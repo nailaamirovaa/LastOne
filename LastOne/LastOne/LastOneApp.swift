214bf21909d0e43f10a48d84b0b21fe128db8d3f
nailaamirovaa <nailaa.amirova@gmail.com>
2026-06-03 09:28:23 +0400

    initial project setup

---
+++ LastOne/LastOne/LastOneApp.swift
@@ -0,0 +1,17 @@
+//
+//  LastOneApp.swift
+//  LastOne
+//
+//  Created by Naila Amirova on 03.06.26.
+//
+
+import SwiftUI
+
+@main
+struct LastOneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LastOne/LastOneTests/LastOneTests.swift
@@ -0,0 +1,17 @@
+//
+//  LastOneTests.swift
+//  LastOneTests
+//
+//  Created by Naila Amirova on 03.06.26.
+//
+
+import Testing
+@testable import LastOne
+
+struct LastOneTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
